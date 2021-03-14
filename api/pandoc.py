69a0e2e37e831175333585a05d4cf26038d77473
Aayush Kumar <ayushk780@gmail.com>
2021-03-14 06:26:55 +0000

    revert api to flask

--- api/pandoc.py
+++ api/pandoc.py
@@ -1,32 +1,35 @@
 import os
 
 # TODO fix mod prem 
-os.system("wget https://t.mytbot.workers.dev/0:/pandoc")
-os.system("chmod +x pandoc")
+# os.system("wget https://t.mytbot.workers.dev/0:/pandoc")
 
-from sanic import Sanic, response
-from sanic_cors import CORS
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
-app = Sanic(__name__)
+app = Flask(__name__)
 CORS(app)
 
+@app.errorhandler(404)
+def not_found(e):
+    return jsonify("This is not the correct page")
+
+
 @app.route("/api/pandoc", methods=["GET", "POST"])
-def pandoc(request):
-    if request.method == "POST":
-        try:    
-            text = str(request.json['inputData'])
-            with open('stext','w') as sft:
-                sft.write(text)
-            
-            cmd = './'+str(request.json['cmd'])
-            os.system(cmd)
-            with open('otext', 'r') as oft:
-                contents = oft.read()
-            return response.json(contents)
-        except Exception as e:
-            return response.json(status=500, content="Request failed please contact admins")
+def pandoc():
+    if request.method == "POST":    
+        text = str(request.json['inputData'])
+        with open('stext','w') as sft:
+            sft.write(text)
+        
+        cmd = './'+str(request.json['cmd'])
+        os.system(cmd)
+        with open('otext', 'r') as oft:
+            contents = oft.read()
+        return jsonify(contents)
+        # print(request.json)
+        # return "HPOST"
 
     if request.method == "GET":
-        return response.json("Hello")
+        return "The api is working as expected"
 
 app.run()
